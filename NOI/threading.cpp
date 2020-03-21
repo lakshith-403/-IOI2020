4d674ce64d9280bafdbf8b2539a37d6be204449e
Lakshith Nishshanke <lakshith.knishshanke@gmail.com>
2020-03-21 14:38:50 +0530

    updated full solution

--- NOI/threading.cpp
+++ NOI/threading.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 list<int> q;
+bool mem[(int)pow(10,8)];
 int main(){
+  // ifstream in("input.in");
   int n;
   cin >> n;
   int i;
@@ -12,9 +14,13 @@ int main(){
     int x;
     cin >> x;
     if(s=="RUN")
+    {
+      if(mem[x]){cout << i << endl; break;}
       q.push_back(x);
+      mem[x] = true;
+    }
     if(s=="END"){
-      if(q.back()==x)q.pop_back();
+      if(q.back()==x){q.pop_back();mem[x]=false;}
       else{
         cout << i << endl;
         break;
