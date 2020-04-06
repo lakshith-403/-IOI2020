a2f5eddbea68a852dd3a7be08e4365dde0ed424d
Lakshith Nishshanke <lakshith.knishshanke@gmail.com>
2020-04-06 20:54:21 +0530

    CF #631 after contest

--- Practise Contests/Codeforces/round 631/probC.cpp
+++ Practise Contests/Codeforces/round 631/probC.cpp
@@ -5,39 +5,34 @@ using namespace std;
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int n,m;
+  long long n,m;
   cin >> n >> m;
-  vector<int> vec;
-  vector<int> l;
-  int sum = 0;
+  vector<long long> vec;
+  vector<long long> l;
+  long long sum = 0;
   l.push_back(-1);
-  int MIN = INT_MAX;
-  for(int i=0;i<m;i++){
-    int L;
+  long long MIN = INT_MAX;
+  for(long long i=0;i<m;i++){
+    long long L;
     cin >> L;
     l.push_back(L);
     sum += L;
   }
-  // sort(l.begin(),l.end());
   if(sum<n){cout << "-1" << "\n";return 0;}
-  int N = n;
+  long long N = n;
   sum++;
-  for(int i=1;i<=m;i++){
-    // cout << "+" << N << "\n";
+  for(long long i=1;i<=m;i++){
     sum -= l.at(i);
-    int j;
-    for(j=N-l.at(i)+1;j>0;j--){
-      // cout << ">" << j << "\n";
+    long long j;
+    for(j=N-l.at(i)+1;j>0;j--)
       if(j<=sum)break;
-    }
-    // cout << "<" << j << "\n";
     vec.push_back(j);
     MIN = min(MIN,j);
     if(j<1){cout << "-1" << "\n";return 0;}
     N = j + l.at(i) - 2;
   }
     if(MIN!=1){cout << "-1" << "\n";return 0;}
-  for(int i=0;i<vec.size()-1;i++)
+  for(long long i=0;i<vec.size()-1;i++)
     cout << vec.at(i) << " ";
   cout << vec.at(vec.size()-1) << "\n";
 }
